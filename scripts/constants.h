0ea8eddd4b110b68a5463f2693dcb2532072ac5d
Viktor Pec <v.pec@sheffield.ac.uk>
2020-06-30 18:22:29 +0100

    moving some constants outside common.icc. This should have gone in 31722d52ae8b4637d7ede9cdfdeda1d0707c2a0d

---
+++ scripts/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double APA_X_POSITIONS[] = {-726.7681, 0., 726.7681};
+const double CPA_X_POSITIONS[] = {-363.38405, 363.38405};
+const double TPC_Z_SIZE = 232.39;
+
+
+
+
+#endif
